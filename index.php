c3f726d8620b3597cc7975d6db1ff1af49cdc942
Kostiantyn Sanduliak <warp682@users.noreply.github.com>
2019-01-11 12:54:11 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
-echo "<--- 3c75 poc --->";
-echo "Headers: ";
+echo "<h2><--- 3c75 poc ---></h2><br>";
+echo "<h3>Headers:</h3> <br>";
 foreach (getallheaders() as $name => $value) {
-    echo "$name: $value\n";
+    echo "<b>$name</b>: $value<br>";
 }
 ?>
