3162aab22bdc1b5d6fd75ffd1fd355f1273d84b9
Kostiantyn Sanduliak <warp682@users.noreply.github.com>
2019-01-11 13:22:10 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 echo "<--- 3c75 poc ---><br>\n";
 echo "You subdomain is vulnerable to subdomain takeower attack!<br>\n";
-echo "What is it? <a href='https://www.hackerone.com/blog/Guide-Subdomain-Takeovers'>Read here</a>"
+echo "What is it? <a href='https://www.hackerone.com/blog/Guide-Subdomain-Takeovers'>Read here</a>";
 echo "<table>";
 foreach (getallheaders() as $name => $value) {
     echo "<tr><td>$name</td><td>$value</td></tr>";
